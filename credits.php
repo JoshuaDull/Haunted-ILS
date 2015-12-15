6b642f05eacb91b60e0682ca4bac6fe7515ae49a
JoshuaDull <jdull@pratt.edu>
2015-12-15 16:11:40 -0500

    added bib records

---
+++ credits.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+require_once 'includes/auth.php';
+require_once 'includes/login.php';
+require_once 'includes/functions.php';
+require_once 'includes/header.php';
+?>
+<body>
+  <div class="game">
+    <h1 id="credits">Alt_Text: A Bibliographic Nightmare</h1><br>
+    <p class="text">This game was created as part of the LIS-664 Web Development class at the Pratt School of Information</p>
+    <br>
+    <h3>Thanks to:</h3><br>
+    <p class="text">
+      <a href="http://billi.nypl.org/">NYPL Labs</a><br><br>
+      <a href="http://www.mattboldt.com/demos/typed-js/">Matt Boldt</a><br><br>
+      <a href="http://opengameart.org/">Open Game Art</a><br><br>
+      <a href="http://www.miskatonic-university.org/">Miskatonic University</a>
+    </p>
+  </div>
+</body>
+</html>
